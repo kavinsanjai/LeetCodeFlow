582a869b0db28f0614279734cb474dbe33c3132e
kavinsanjai <164164992+kavinsanjai@users.noreply.github.com>
2025-12-03 23:14:04 +0530

    Time: 119 ms (63.51%), Space: 78.3 MB (23.51%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -6,25 +6,24 @@ class Solution(object):
         count=0
         dirs=((1,0),(0,1),(-1,0),(0,-1))
         def dfs(i,j):
-            visited.add((i,j))
             grid[i][j]=0
             for di,dj in dirs:
                 ni,nj=i+di,j+dj
-                if 0<=ni<rows and 0<=nj<cols and (ni,nj) not in visited and grid[ni][nj]==1:
+                if 0<=ni<rows and 0<=nj<cols and  grid[ni][nj]==1:
                     dfs(ni,nj)
 
         for j in range(0,cols):
-            if (0,j) not in visited and grid[0][j]==1:
+            if grid[0][j]==1:
                 dfs(0,j)
         for i in range(0,rows):
-            if(i,0) not in visited and grid[i][0]==1:
+            if grid[i][0]==1:
                 dfs(i,0)
         
         for j in range(0,cols):
-            if(rows-1,j) not in visited and grid[rows-1][j]==1:
+            if grid[rows-1][j]==1:
                 dfs(rows-1,j)
         for i in range(0,rows):
-            if(i,cols-1) not in visited and grid[i][cols-1]==1:
+            if grid[i][cols-1]==1:
                 dfs(i,cols-1)
         
 
