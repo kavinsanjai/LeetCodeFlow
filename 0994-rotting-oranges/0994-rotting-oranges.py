4655259e63dc6b83a25ab245971789a24e92677d
kavinsanjai <164164992+kavinsanjai@users.noreply.github.com>
2025-12-03 14:54:22 +0530

    Time: 8 ms (33.75%), Space: 12.5 MB (16.8%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def orangesRotting(self, grid):
+        visited=set()
+        dirs=((1,0),(0,1),(-1,0),(0,-1))
+        q=deque()
+        mini=0
+        fresh=0
+
+        for i in range(0,len(grid)):
+            for j in range(0,len(grid[0])):
+                if grid[i][j]==2:
+                    q.append((i,j))
+                elif grid[i][j]==1:
+                    fresh+=1
+      
+        while q and fresh>0:
+            for _ in range(len(q)):
+                i,j=q.popleft()
+                for di,dj in dirs:
+                    ni=di+i
+                    nj=dj+j
+                    if 0<=ni<len(grid) and 0<=nj<len(grid[0])  and grid[ni][nj]==1:
+                        q.append((ni,nj))
+                        fresh-=1
+                        grid[ni][nj]=2
+            mini+=1
+        
+        if fresh>0:
+            return -1
+        else:
+            return mini
+        
+        
+                
