0eae0a8091c1107e37ac7bbf7ccefbea76b65cf6
kavinsanjai <164164992+kavinsanjai@users.noreply.github.com>
2025-12-09 19:14:54 +0530

    Time: 11 ms (91.21%), Space: 12.3 MB (98.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,6 +4,6 @@ class Solution(object):
         dp[0]=0
         if n>=1:
             dp[1]=1
-            for i in range(2,n+1):
-                dp[i]=dp[i-1]+dp[i-2]
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
