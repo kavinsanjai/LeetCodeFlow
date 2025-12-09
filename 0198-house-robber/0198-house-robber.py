ca1dd2214c2b57c5cce4d14a0175d625902ebe24
kavinsanjai <164164992+kavinsanjai@users.noreply.github.com>
2025-12-09 19:46:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (82.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def rob(self, nums):
-        rob1, rob2 = 0, 0
-        for n in nums:
-            temp=max(rob1+n,rob2)
-            rob1=rob2
-            rob2=temp
-        return rob2
-
+        dp=[0]*(len(nums)+1)
+        dp[0]=nums[0]
+        if len(nums)>1:
+            dp[1]=max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+        return dp[len(nums)-1]
         
