405a1bfc1a03636f254e154e0ecae6a99eb86161
kavinsanjai <164164992+kavinsanjai@users.noreply.github.com>
2025-12-12 12:55:36 +0530

    Time: 13 ms (10.88%), Space: 12.4 MB (58.59%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        paragraph=re.sub('[^\w]',' ',paragraph.lower())
+        paragraph=paragraph.split()
+        fre=Counter(paragraph)
+        fre = sorted(fre.items(), key=lambda x: x[1], reverse=True)
+        print(fre)
+        for i,j in fre:
+            if i not in banned:
+                return i
+        
+        
