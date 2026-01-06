d305a663ec7da4930ee1b920a92e94e2872bec56
kavinsanjai <164164992+kavinsanjai@users.noreply.github.com>
2026-01-06 11:35:09 +0530

    Time: 3 ms (98.41%), Space: 13.6 MB (61.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,38 @@
 class Solution(object):
     def trap(self, height):
-        maxleft = [0]*len(height)
-        maxright = [0]*len(height)
+        # maxleft = [0]*len(height)
+        # maxright = [0]*len(height)
 
-        maxleft[0] = height[0]
-        for i in range(1, len(height)):
-            maxleft[i] = max(height[i], maxleft[i-1])
+        # maxleft[0] = height[0]
+        # for i in range(1, len(height)):
+        #     maxleft[i] = max(height[i], maxleft[i-1])
 
-        maxright[len(height)-1] = height[len(height)-1]
-        for i in range(len(height)-2, -1, -1):
-            maxright[i] = max(height[i], maxright[i+1])
-        total_water=0
-        for i in range(0,len(height)):
-            total_water+=min(maxright[i],maxleft[i])-height[i]
-        return total_water
+        # maxright[len(height)-1] = height[len(height)-1]
+        # for i in range(len(height)-2, -1, -1):
+        #     maxright[i] = max(height[i], maxright[i+1])
         
+        # total_water=0
+        # for i in range(0,len(height)):
+        #     total_water+=min(maxright[i],maxleft[i])-height[i]
+        # return total_water
+        left=0
+        right=len(height)-1
+        max_left=0
+        max_right=0
+        total_water=0
+        while(left<right):
+            if height[left]<=height[right]:
+                if height[left]>=max_left:
+                    max_left=height[left]
+                else:
+                    total_water+=max_left-height[left]
+                left+=1
+            else:
+                if height[right]>=max_right:
+                    max_right=height[right]
+                else:
+                    total_water+=max_right-height[right]
+                right-=1
+
+        return total_water
         
