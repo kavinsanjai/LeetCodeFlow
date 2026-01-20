ed2bb75076eeaae0bc01a6935fc6e07105438a82
kavinsanjai <164164992+kavinsanjai@users.noreply.github.com>
2026-01-20 20:25:39 +0530

    Time: 25 ms (62.5%), Space: 12.6 MB (25%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
@@ -4,13 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # ans=[]
+        # for i in range(0,len(nums)):
+        #     for j in range(1,nums[i]):
+        #         if j|j+1 == nums[i]:
+        #             ans.append(j)
+        #             break
+        #     else:
+        #         ans.append(-1)
+        # return ans
         ans=[]
         for i in range(0,len(nums)):
-            for j in range(1,nums[i]):
-                if j|j+1 == nums[i]:
-                    ans.append(j)
-                    break
+            for j in range(31,-1,-1):
+                if nums[i]&(1<<j):
+                    x=nums[i]&~(1<<j)
+                    if x|x+1 == nums[i]:
+                        ans.append(x)
+                        break
             else:
                 ans.append(-1)
         return ans
+
         
