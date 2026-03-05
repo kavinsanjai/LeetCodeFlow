87fd3364f3305091ab1bb1d8dde681ea9a80f6df
kavinsanjai <164164992+kavinsanjai@users.noreply.github.com>
2026-03-05 10:25:13 +0530

    Time: 19 ms (43.01%), Space: 12.8 MB (13.98%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minOperations(self, s):
+        count_0=('01'*len(s))[:len(s)]
+        print(count_0)
+        count_1=('10'*len(s))[:len(s)]
+        print(count_1)
+        s_count,f_count=0,0
+        for i in range(0,len(s)):
+            if s[i]!=count_0[i]:
+                f_count+=1
+            if s[i]!=count_1[i]:
+                s_count+=1
+        return min(f_count,s_count)
+
+
+        
